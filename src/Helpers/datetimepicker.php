902e02226ba4096f5c2e91f50d3eb93335b86c0e
rafazingano <rafazingano@gmail.com>
2020-01-24 17:13:35 -0300

    Commit all

---
+++ src/Helpers/datetimepicker.php
@@ -0,0 +1,9 @@
+<?php
+if (!function_exists('datetimepicker')) {
+
+    function datetimepicker($class)
+    {
+        return Form::text('email', 'example@gmail.com');;
+    }
+
+}
--- src/Providers/VendorServiceProvider.php
+++ src/Providers/VendorServiceProvider.php
@@ -8,25 +8,14 @@ use Illuminate\Support\ServiceProvider;
 class VendorServiceProvider extends ServiceProvider
 {
 
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/../Views', 'vendor');
-
         Blade::directive('datetime', function ($expression) {
             return "<?php echo ($expression)->format('m/d/Y H:i'); ?>";
         });
     }
 
-    /**
-     * Register services.
-     *
-     * @return void
-     */
     public function register()
     {
 

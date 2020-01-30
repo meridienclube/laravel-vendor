0b1611a589dd19aa3d8d84a13841d3e2380fabdd
rafazingano <rafazingano@gmail.com>
2020-01-30 16:36:33 -0300

    Update all

--- src/Providers/VendorServiceProvider.php
+++ src/Providers/VendorServiceProvider.php
@@ -11,8 +11,17 @@ class VendorServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/../Views', 'vendor');
+
         Blade::directive('datetime', function ($expression) {
-            return "<?php echo ($expression)->format('m/d/Y H:i'); ?>";
+            return "<?php echo ($expression)->format('d/m/Y H:i'); ?>";
+        });
+
+        Blade::directive('date', function ($expression) {
+            return "<?php echo ($expression)->format('d/m/Y'); ?>";
+        });
+
+        Blade::directive('time', function ($expression) {
+            return "<?php echo ($expression)->format('H:i'); ?>";
         });
     }
 
